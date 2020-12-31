f5badb43511a30e7547a18aaec07d3a540865407
Hila Caspi <caspi.home@gmail.com>
2020-12-31 14:22:18 -0800

    initial db setup

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+// Initializing connection to database locally or through JAWSDB->Heroku
+let sequelize;
+
+// routing to remote or local db
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  Sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  });
+}
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize");
+
+const sess = {
+  secret: "super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Setting static files to the public dir
+app.use(express.static(path.join(__dirname, "public")));
+
+// Setting handlebars as the default template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Turn on routes
+app.use(routes);
+
+// Turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
