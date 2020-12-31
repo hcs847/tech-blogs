fd50b2f10e6b182347c3edd6a6b4f7117f60fc09
Hila Caspi <caspi.home@gmail.com>
2020-12-31 14:49:55 -0800

    updating basic setup typos

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const path = require("path");
 const express = require("express");
-const routes = require("./controllers");
+
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
+
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.port || 3001;
 
 const session = require("express-session");
 
-const SequelizeStore = require("connect-session-sequelize");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: "super secret secret",
@@ -33,10 +35,4 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// Turn on routes
-app.use(routes);
-
-// Turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-});
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
